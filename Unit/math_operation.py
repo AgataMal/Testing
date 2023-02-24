782e13f2249539392665b0f4397fa4370faf4a79
Agata <malczyk.agata88@gmail.com>
2023-02-24 12:36:42 +0100

    Unit tests

--- Unit/math_operation.py
+++ Unit/math_operation.py
@@ -1,6 +1,15 @@
 def sum_result(nums):
     result = 0
     for x in nums:
-        result=x+result
+        result = x+result
         print(result)
     return result
+
+
+def czy_liczba_pierwsza(n):
+    if n==1:
+        return False
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
--- Unit/unit_test.py
+++ Unit/unit_test.py
@@ -1,10 +1,19 @@
 import unittest
 import math_operation
 
+
 class UnitTests(unittest.TestCase):
-    
+
     def test_sum_result(self):
-        sum_result=math_operation.sum_result([2,3,4,5])
-        self.assertEqual(sum_result,14)
+        sum_result = math_operation.sum_result([2, 3, 4, 5])
+        self.assertEqual(sum_result, 14)
 
-
+    def test_czy_liczba_pierwsza(self):
+        result = math_operation.czy_liczba_pierwsza(5)
+        self.assertTrue(result)
+        result = math_operation.czy_liczba_pierwsza(4)
+        self.assertFalse(result)
+        result = math_operation.czy_liczba_pierwsza(7)
+        self.assertTrue(result)
+        result = math_operation.czy_liczba_pierwsza(1)
+        self.assertFalse(result)
