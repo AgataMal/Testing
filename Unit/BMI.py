9d60cbad1a8a05b3186d4baad5bf7ad6ec2bfca3
Agata <malczyk.agata88@gmail.com>
2023-02-26 14:05:09 +0100

    new unit test

--- Unit/BMI.py
+++ Unit/BMI.py
@@ -1,14 +1,21 @@
-def czy_gruby(wzrost,waga):
-    BMI=waga/wzrost**2
+def czy_gruby(wzrost, waga):
+    if wzrost is None or waga is None:
+        return "błąd"
+    if type(wzrost) == str or type(waga) == str:
+        return "błąd"
+    if wzrost <= 0 and waga <= 0:
+        return "błąd"
 
-    if BMI <18.5:
+    BMI = waga/wzrost**2
+
+    if BMI < 18.5:
         return "niedowaga"
     else:
-        if BMI>=18.5 and BMI<=24.9:
+        if BMI >= 18.5 and BMI <= 24.9:
             return "norma"
-        elif BMI>=25 and BMI<=29.9:
+        elif BMI >= 25 and BMI <= 29.9:
             return "nadwaga"
-        elif BMI>=30 and BMI<=34.9:
+        elif BMI >= 30 and BMI <= 34.9:
             return "otyłość"
-        elif BMI>35:
+        elif BMI > 35:
             return "otyłość kliniczna"
--- Unit/BMI_test.py
+++ Unit/BMI_test.py
@@ -1,10 +1,19 @@
 import unittest
 import BMI
 
+
 class UnitTests(unittest.TestCase):
     def test_wynik_BMI(self):
-        self.assertEqual(BMI.czy_gruby(1.5, 30),'niedowaga')
-        self.assertEqual(BMI.czy_gruby(1.5, 100),'otyłość kliniczna')
-        self.assertEqual(BMI.czy_gruby(1.5, 50),'norma')
-        self.assertEqual(BMI.czy_gruby(1.5, 70),'otyłość')
+        self.assertEqual(BMI.czy_gruby(1.5, 30), 'niedowaga')
+        self.assertEqual(BMI.czy_gruby(1.5, 100), 'otyłość kliniczna')
+        self.assertEqual(BMI.czy_gruby(1.5, 50), 'norma')
+        self.assertEqual(BMI.czy_gruby(1.5, 70), 'otyłość')
 
+        self.assertEqual(BMI.czy_gruby(0, 0), "błąd")
+        self.assertEqual(BMI.czy_gruby(1.5, None), "błąd")
+        self.assertEqual(BMI.czy_gruby(None, None), "błąd")
+        self.assertEqual(BMI.czy_gruby(None, 20), "błąd")
+
+        self.assertEqual(BMI.czy_gruby('@#$', '@@#$'), "błąd")
+        self.assertEqual(BMI.czy_gruby(1.5, '@@#'), "błąd")
+        self.assertEqual(BMI.czy_gruby('@#$', 200), "błąd")
