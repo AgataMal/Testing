cefe153997aea2a16c5afba166317ba946d1881f
Agata <malczyk.agata88@gmail.com>
2023-03-12 20:18:33 +0100

    Import Java

---
+++ Unit/java/testing/src/main/java/learn/testing/App.java
@@ -0,0 +1,13 @@
+package learn.testing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
