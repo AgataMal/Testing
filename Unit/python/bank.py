cde02f3e5f51906556a310636b714c6c49e5aaa5
Agata <malczyk.agata88@gmail.com>
2023-03-13 21:23:33 +0100

    encapsulate

--- Unit/python/bank.py
+++ Unit/python/bank.py
@@ -1,11 +1,14 @@
 class Bank:
     def __init__(self, money=0) -> None:
-        self.amount = money
+        self.__balance = money
 
     def add_money(self, money):
-        self.amount += money
-        return self.amount
+        self.__balance += money
+        return self.__balance
 
     def withdraw_money(self, money):
-        self.amount -= money
-        return self.amount
+        self.__balance -= money
+        return self.__balance
+
+    def get_balance(self):
+        return self.__balance
