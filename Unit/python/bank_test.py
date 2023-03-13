6f9163911a879affbfd128c28241021719d22294
AgataMal <98488410+AgataMal@users.noreply.github.com>
2023-03-13 21:24:57 +0100

    encapsulate (#6)

--- Unit/python/bank_test.py
+++ Unit/python/bank_test.py
@@ -1,17 +1,17 @@
 import unittest
-import bank
+from bank import *
 
 
 class UnitTests(unittest.TestCase):
     def test_create_bank(self):
-        bankPKO = bank.Bank()
-        self.assertIsInstance(bankPKO, bank.Bank)
-        self.assertEqual(bankPKO.amount, 0)
+        bankPKO = Bank()
+        self.assertIsInstance(bankPKO, Bank)
+        self.assertEqual(bankPKO.get_balance(), 0)
 
     def test_add_money(self):
-        bankPKO = bank.Bank()
-        self.assertEqual(bankPKO.add_money(100), 100 )
+        bankPKO = Bank()
+        self.assertEqual(bankPKO.add_money(100), 100)
 
     def test_withdraw_money(self):
-        bankPKO = bank.Bank(200)
-        self.assertEqual(bankPKO.withdraw_money(100), 100 )
+        bankPKO = Bank(200)
+        self.assertEqual(bankPKO.withdraw_money(100), 100)
