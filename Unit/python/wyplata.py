bdc22fb2e0c10bdc2b7c7d99e3ca1138e1de2754
Agata <malczyk.agata88@gmail.com>
2023-03-18 13:11:42 +0100

    error handling 'wyplata'

--- Unit/python/wyplata.py
+++ Unit/python/wyplata.py
@@ -1,6 +1,9 @@
 def wyplac(kwota, kod_pin):
     stan_konta = 1000
+    poprawny_pin = 2345
+    if not poprawny_pin == kod_pin:
+        raise ValueError("błędny pin")
     if kwota <= stan_konta and kwota > 0:
-        poprawny_pin = 2345
-        if poprawny_pin == kod_pin:
-            return kwota
+        return kwota
+    else:
+        raise ValueError("błędna kwota")
--- Unit/python/wyplata_test.py
+++ Unit/python/wyplata_test.py
@@ -5,6 +5,10 @@ class UnitTests(unittest.TestCase):
 
     def test_wyplac(self):
         self.assertEqual(wyplata.wyplac(50, 2345), 50)
-        self.assertEqual(wyplata.wyplac(2000, 2345), None)
-        self.assertEqual(wyplata.wyplac(50, 1345), None)
-        self.assertEqual(wyplata.wyplac(-50, 2345), None)  
+        with self.assertRaisesRegex(ValueError, "błędna kwota"):
+            wyplata.wyplac (2000, 2345)
+        with self.assertRaisesRegex(ValueError, "błędny pin"):
+            wyplata.wyplac (50, 1345)
+        with self.assertRaisesRegex(ValueError, "błędna kwota"):
+            wyplata.wyplac (-50, 2345)         
+        
