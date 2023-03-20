fcd0fe1fe18be209206f83cceaba9309ac8ea0f3
Agata <malczyk.agata88@gmail.com>
2023-03-20 14:42:01 +0100

    new bank tasks 'cases'

--- Unit/python/bank.py
+++ Unit/python/bank.py
@@ -3,12 +3,20 @@ class Bank:
         self.__balance = money
 
     def add_money(self, money):
-        self.__balance += money
-        return self.__balance
+        if money > 0:
+            self.__balance += money
+            return self.__balance
+        else:
+            raise ValueError("podaj kwotę większą od 0")
 
     def withdraw_money(self, money):
-        self.__balance -= money
-        return self.__balance
+        if not money % 1 == 0:
+            raise ValueError("tylko liczba całkowita")
+        if self.__balance - money > 0:
+            self.__balance -= money
+            return self.__balance
+        else:
+            raise ValueError("kwota przeracza stan konta")
 
     def get_balance(self):
         return self.__balance
