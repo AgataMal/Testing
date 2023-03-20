75de6e9c8d6e01a92b0e8d262e2b3d1dccebf36e
AgataMal <98488410+AgataMal@users.noreply.github.com>
2023-03-20 14:42:36 +0100

    new bank tasks 'cases' (#14)

--- Unit/python/bank_test.py
+++ Unit/python/bank_test.py
@@ -11,7 +11,13 @@ class UnitTests(unittest.TestCase):
     def test_add_money(self):
         bankPKO = Bank()
         self.assertEqual(bankPKO.add_money(100), 100)
-
+        with self.assertRaisesRegex(ValueError, "podaj kwotę większą od 0"):
+            bankPKO.add_money(0)
+            
     def test_withdraw_money(self):
         bankPKO = Bank(200)
         self.assertEqual(bankPKO.withdraw_money(100), 100)
+        with self.assertRaisesRegex(ValueError, "kwota przeracza stan konta"):
+            bankPKO.withdraw_money(300)
+        with self.assertRaisesRegex(ValueError, "tylko liczba całkowita"):
+            bankPKO.withdraw_money(0.01)
