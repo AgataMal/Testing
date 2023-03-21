c3c7c5d34806737b0febc414049335bc110b0228
Agata <malczyk.agata88@gmail.com>
2023-03-21 20:27:14 +0100

    bmi test update

--- Unit/python/BMI_test.py
+++ Unit/python/BMI_test.py
@@ -1,19 +1,30 @@
 import unittest
-import BMI
+from BMI import wynik_BMI
 
 
 class UnitTests(unittest.TestCase):
     def test_wynik_BMI(self):
-        self.assertEqual(BMI.czy_gruby(1.5, 30), 'niedowaga')
-        self.assertEqual(BMI.czy_gruby(1.5, 100), 'otyłość kliniczna')
-        self.assertEqual(BMI.czy_gruby(1.5, 50), 'norma')
-        self.assertEqual(BMI.czy_gruby(1.5, 70), 'otyłość')
+        self.assertEqual(wynik_BMI(1.5, 30), 'niedowaga')
+        self.assertEqual(wynik_BMI(1.5, 100), 'otyłość kliniczna')
+        self.assertEqual(wynik_BMI(1.5, 50), 'norma')
+        self.assertEqual(wynik_BMI(1.5, 70), 'otyłość')
 
-        self.assertEqual(BMI.czy_gruby(0, 0), "błąd")
-        self.assertEqual(BMI.czy_gruby(1.5, None), "błąd")
-        self.assertEqual(BMI.czy_gruby(None, None), "błąd")
-        self.assertEqual(BMI.czy_gruby(None, 20), "błąd")
+        expected_non_empty_input="wprowadź obie wartości liczbowe"
 
-        self.assertEqual(BMI.czy_gruby('@#$', '@@#$'), "błąd")
-        self.assertEqual(BMI.czy_gruby(1.5, '@@#'), "błąd")
-        self.assertEqual(BMI.czy_gruby('@#$', 200), "błąd")
+        with self.assertRaisesRegex(ValueError, "podaj wartość większą niż 0"):
+            wynik_BMI(0, 0)
+        with self.assertRaisesRegex(ValueError, expected_non_empty_input):
+            wynik_BMI(None, None)
+        with self.assertRaisesRegex(ValueError, expected_non_empty_input):
+            wynik_BMI(1.5, None)
+        with self.assertRaisesRegex(ValueError, expected_non_empty_input):
+            wynik_BMI(None, 20)
+        
+        expected_valid_input="błędne wartości"
+
+        with self.assertRaisesRegex(ValueError, expected_valid_input):
+            wynik_BMI('@#$', '@@#$')
+        with self.assertRaisesRegex(ValueError, expected_valid_input):
+            wynik_BMI(1.5, '@@#')
+        with self.assertRaisesRegex(ValueError, expected_valid_input):
+            wynik_BMI('@#$', 200)
