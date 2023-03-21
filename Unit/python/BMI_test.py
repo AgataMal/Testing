b57b37e7f4fcc00624ab93d22c05383b9baff006
Agata <malczyk.agata88@gmail.com>
2023-03-21 20:33:17 +0100

    bmi test update

--- Unit/python/BMI_test.py
+++ Unit/python/BMI_test.py
@@ -3,16 +3,16 @@ from BMI import wynik_BMI
 
 
 class UnitTests(unittest.TestCase):
-    def test_wynik_BMI(self):
+    def test_should_return_valid_output(self):
         self.assertEqual(wynik_BMI(1.5, 30), 'niedowaga')
         self.assertEqual(wynik_BMI(1.5, 100), 'otyłość kliniczna')
         self.assertEqual(wynik_BMI(1.5, 50), 'norma')
         self.assertEqual(wynik_BMI(1.5, 70), 'otyłość')
 
+    def test_should_fail_on_empty_input(self):
+
         expected_non_empty_input="wprowadź obie wartości liczbowe"
 
-        with self.assertRaisesRegex(ValueError, "podaj wartość większą niż 0"):
-            wynik_BMI(0, 0)
         with self.assertRaisesRegex(ValueError, expected_non_empty_input):
             wynik_BMI(None, None)
         with self.assertRaisesRegex(ValueError, expected_non_empty_input):
@@ -20,6 +20,8 @@ class UnitTests(unittest.TestCase):
         with self.assertRaisesRegex(ValueError, expected_non_empty_input):
             wynik_BMI(None, 20)
         
+    def test_should_fail_on_string_input(self):
+
         expected_valid_input="błędne wartości"
 
         with self.assertRaisesRegex(ValueError, expected_valid_input):
@@ -28,3 +30,8 @@ class UnitTests(unittest.TestCase):
             wynik_BMI(1.5, '@@#')
         with self.assertRaisesRegex(ValueError, expected_valid_input):
             wynik_BMI('@#$', 200)
+
+    def test_should_fail_on_zero_input(self):
+
+        with self.assertRaisesRegex(ValueError, "podaj wartość większą niż 0"):
+            wynik_BMI(0, 0)
