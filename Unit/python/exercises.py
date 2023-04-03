ca41fe1f7e2853635a27dab9d2dd41eb7368d61d
Agata <malczyk.agata88@gmail.com>
2023-04-03 21:06:48 +0200

    hackerrank

--- Unit/python/exercises.py
+++ Unit/python/exercises.py
@@ -6,11 +6,39 @@
 def test_is_adult():
     assert is_adult(20)
 '''
+
+
 def is_adult(age):
-    if age>=18:
+    if age >= 18:
         return True
-print (is_adult(20))
 
 
-def triangle_field(base, height)->float:
+print(is_adult(20))
+
+
+def triangle_field(base, height) -> float:
     return 0.5 * base * height
+
+
+def is_Weird(n):
+    if n % 2 != 0:
+        print("Weird")
+    elif n % 2 == 0 and n >= 2 and n <= 5:
+        print("Not Weird")
+    elif n % 2 != 0 and n >= 6 and n < 20:
+        print("Not Weird")
+    else:
+        print("Weird")
+
+
+def is_leap(year):
+    if year % 4 == 0: 
+        if year %100==0:
+            if year %400 == 0:
+                return True
+            return False
+        return True
+    return False
+    
+
+
--- Unit/python/exercises_test.py
+++ Unit/python/exercises_test.py
@@ -13,4 +13,16 @@ class UnitTests(unittest.TestCase):
     def test_triangle_field(self):
         self.assertEqual(exercises.triangle_field (10,15),75)
 
-    
+    def test_is_Weird(self):
+        exercises.is_Weird(24)
+
+    def test_is_leap(self):
+        self.assertTrue(exercises.is_leap(2000))
+        self.assertTrue(exercises.is_leap(2400))
+        self.assertFalse(exercises.is_leap(1900))
+        self.assertFalse(exercises.is_leap(1800))
+        self.assertFalse(exercises.is_leap(2100))
+        self.assertFalse(exercises.is_leap(2200))
+        self.assertFalse(exercises.is_leap(2300))
+        self.assertFalse(exercises.is_leap(2500))
+        
